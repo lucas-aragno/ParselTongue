2bfa17024c73de12065776722ba2b4d9f4e5064c
Lucas Aragno <lucas.aragno@moove-it.com>
2016-06-11 02:57:11 -0300

    Add excersies

---
+++ chapter-1/data-structures/test.py
@@ -0,0 +1,11 @@
+# create two lists and join them
+
+
+# create a tuple and print the first and the last element
+
+
+# create a ron dict w/ this data: name, hair_color, and best_friend
+
+
+
+# get the dict from the last point and add a new key called last_name w/ ron's last name
